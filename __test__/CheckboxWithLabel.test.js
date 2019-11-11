6dfd211cc4657816fd79dd086b41ac2833d9131d
Jack Hu <hutufeia2009@sina.com>
2019-11-11 18:05:31 +0800

    add test

--- __test__/CheckboxWithLabel.test.js
+++ __test__/CheckboxWithLabel.test.js
@@ -1,7 +1,7 @@
 import React, {createRef} from 'react'
 
 import * as TestUtils from 'react-dom/test-utils'
-import CheckboxWithLabel from '../CheckboxWithLabel'
+import CheckboxWithLabel from '../src/CheckboxWithLabel'
 
 
 it('CheckboxWithLabel changes the text after click', () => {
