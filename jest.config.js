a8f00e3daeb2d6806ef4d0ce29c98e4144cd7466
Jack Hu <hutufeia2009@sina.com>
2019-11-11 11:16:42 +0800

    添加typescript 支持

--- jest.config.js
+++ jest.config.js
@@ -168,7 +168,9 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   // transform: null,
-
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/"
